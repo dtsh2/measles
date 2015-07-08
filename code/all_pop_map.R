4c8d9b18d2283adcf6c07d717a0fc44c4506f4bb
Jonathan Marshall <jmarshall@never.you.mind>
2015-07-08 17:23:05 +1200

    update all_pop_map.R for corrected figures

--- code/all_pop_map.R
+++ code/all_pop_map.R
@@ -3,69 +3,37 @@ library(classInt)
 
 data_denom<-read.csv("2013-mb-dataset-Total-New-Zealand-individual-part-1_AU.csv",header=T)
 
+# expand the age groups to individual ages by assuming that all ages are equally likely
+# NOTE: This analysis only uses those over the age of 10 and ignores all others
+
+cols <- paste("AGE",seq(10,60,by=5),seq(10,60,by=5)+4, sep="_")
+au_ages <- data_denom[,rep(cols, each=5)]/5
+au_ages <- cbind(au_ages, data_denom[,"AGE_65_OVER"])
+names(au_ages) <- 10:65
+
+# multiply up by population immunity levels
+# TODO: THIS IS REPEATED CODE - ELIMINATE IT!!!
 popimmune<-read.csv("data/PopnImmunityAll.csv",header=T)
-#popimmune$Age = factor(popimmune$Age,levels(popimmune$Age)[c(2,3,6,8,10:12,4,5,7,9,1)])
+pop<-read.csv("data/popnsize.csv",header=T)
+colnames(pop)<-0:100
+# expand popimmune out to years 0..100
+NaiveByYear <- data.frame(Age=0:100, Population=t(pop), Immunity=NA)
+for (i in 1:nrow(popimmune)) {
+  start <- popimmune$AgeStart[i]+1
+  end   <- ifelse(is.finite(popimmune$AgeEnd[i]), popimmune$AgeEnd[i]+1, nrow(NaiveByYear))
+  NaiveByYear$Immunity[start:end] <- popimmune$Immunity[i]
+}
 
-data_denom$AGE_10_14_im <- 4/5 * data_denom$AGE_10_14 * popimmune[7,3] + 1/5  * data_denom$AGE_10_14 * popimmune[8,3]
-data_denom$AGE_15_19_im <- 4/5 * data_denom$AGE_15_19 * popimmune[8,3] + 1/5  * data_denom$AGE_15_19 * popimmune[9,3]
-data_denom$AGE_20_24_im <- 4/5 * data_denom$AGE_20_24 * popimmune[9,3] + 1/5  * data_denom$AGE_20_24 * popimmune[10,3]
-data_denom$AGE_25_29_im <- 4/5 * data_denom$AGE_25_29 * popimmune[10,3] + 1/5  * data_denom$AGE_25_29 * popimmune[11,3]
-data_denom$AGE_30_34_im <-  data_denom$AGE_30_34  * popimmune[11,3]
-data_denom$AGE_35_39_im <-  data_denom$AGE_35_39  * popimmune[11,3]
-data_denom$AGE_40_44_im <-  data_denom$AGE_40_44  * popimmune[11,3]
-data_denom$AGE_45_49_im <-  data_denom$AGE_45_49  * popimmune[11,3]
-data_denom$AGE_50_54_im <-  3/5 * data_denom$AGE_50_54 * popimmune[11,3] + 2/5  * data_denom$AGE_10_14 * popimmune[12,3]
-data_denom$AGE_55_59_im <-  data_denom$AGE_55_59  * popimmune[12,3]
-data_denom$AGE_60_64_im <-  data_denom$AGE_60_64 * popimmune[12,3]
-data_denom$AGE_65_OVER_im <- data_denom$AGE_65_OVER * popimmune[12,3]
+immunity <- (NaiveByYear %>% filter(Age >= 10 & Age <= 65) %>% select(Immunity))[,1]
+au_imm <- sweep(au_ages, 2, immunity, "*")
 
-# data_denom$AGE_10_14_pc <- data_denom$AGE_10_14_im / data_denom$AGE_10_14
-# data_denom$AGE_15_19_pc <- data_denom$AGE_15_19_im / data_denom$AGE_15_19
-# data_denom$AGE_20_24_pc <- data_denom$AGE_20_24_im / data_denom$AGE_20_24
-# data_denom$AGE_25_29_pc <- data_denom$AGE_25_29_im / data_denom$AGE_25_29
-# data_denom$AGE_30_34_pc <- data_denom$AGE_30_34_im / data_denom$AGE_30_34
-# data_denom$AGE_35_39_pc <- data_denom$AGE_35_39_im / data_denom$AGE_35_39
-# data_denom$AGE_40_44_pc <- data_denom$AGE_40_44_im / data_denom$AGE_40_44
-# data_denom$AGE_45_49_pc <- data_denom$AGE_45_49_im / data_denom$AGE_45_49
-# data_denom$AGE_50_54_pc <- data_denom$AGE_50_54_im / data_denom$AGE_50_54
-# data_denom$AGE_55_59_pc <- data_denom$AGE_55_59_im / data_denom$AGE_55_59
-# data_denom$AGE_60_64_pc <- data_denom$AGE_60_64_im / data_denom$AGE_60_64
-# data_denom$AGE_65_OVER_pc <- data_denom$AGE_65_OVER_im / data_denom$AGE_65_OVER
-
-data_denom$AGE_TOTAL_im <- 
-  
-data_denom$AGE_10_14_im+
-data_denom$AGE_15_19_im+
-data_denom$AGE_20_24_im+
-data_denom$AGE_25_29_im+
-data_denom$AGE_30_34_im+
-data_denom$AGE_35_39_im+
-data_denom$AGE_40_44_im+
-data_denom$AGE_45_49_im+
-data_denom$AGE_50_54_im+
-data_denom$AGE_55_59_im+
-data_denom$AGE_60_64_im+
-data_denom$AGE_65_OVER_im
-
-data_denom$AGE_TOTAL_dh <- 
-  
-  data_denom$AGE_10_14+
-  data_denom$AGE_15_19+
-  data_denom$AGE_20_24+
-  data_denom$AGE_25_29+
-  data_denom$AGE_30_34+
-  data_denom$AGE_35_39+
-  data_denom$AGE_40_44+
-  data_denom$AGE_45_49+
-  data_denom$AGE_50_54+
-  data_denom$AGE_55_59+
-  data_denom$AGE_60_64+
-  data_denom$AGE_65_OVER
-
-
-data_denom$pc <- data_denom$AGE_TOTAL_im / data_denom$AGE_TOTAL_dh
+data_denom$pc <- rowSums(au_imm) / rowSums(au_ages)
 
 ###############
+## MAPS HERE
+###############
+
+map_dir <- "figures"
 
 library(maptools)
 library(lubridate)
@@ -74,21 +42,14 @@ library(dplyr)
 # read in shapefile for AU's
 au <- readShapeSpatial("AU2013_GV_Full")
 
-# read in domicile <-> AU map
-data_dom <- read.csv("NIR_MMR_data_20060101-20140731/domicile_code.csv")
+data_au <- slot(au, "data") %>% mutate(AU_CODE = as.numeric(as.character(AU2013)))
+data_au <- data_au %>% left_join(data_denom)
 
-testgp <- data_denom %>% mutate(area.unit=AU_CODE)
-testgp <- testgp %>% left_join(data_dom)
-
-testgp <- testgp %>% mutate(AU2013 = area.unit)
-
-testgp <- testgp %>% mutate(dom = DOMICILE_CODE)
-# grouped_all_years <- grouped_all_years %>% left_join(data_denom)
-
-data_au <- slot(au, "data") %>% mutate(AU2013 = as.numeric(as.character(AU2013)))
-data_au <- data_au %>% left_join(testgp)
-
-in_the_sea <- grepl("^Oceanic", data_au$AU2013_NAM)
+in_the_sea <- grepl("^Oceanic\\-", data_au$AU2013_NAM)
+tidal      <- grepl("^Tidal\\-", data_au$AU2013_NAM)
+inlet      <- grepl("^Inlet[s]*\\-", data_au$AU2013_NAM)
+inland_water <- grepl("^Inland Water\\-", data_au$AU2013_NAM)
+no_area    <- data_au$LAND_AREA_ == 0
 
 ## plot and save
 
@@ -104,63 +65,45 @@ breaks <- cut(data_au$pc, fixedBreaks)
 pal<-c(heat.colors(3),"green","blue")
 cols <- pal[breaks]
 
-cols[in_the_sea] <- NA
-par(mfrow=c(1,1))
+cols[in_the_sea | tidal | inlet | inland_water | no_area] <- NA
 
 #####
 
-pdf(paste("census_immunity_age.pdf"), width=7, height=5)
-
-plot(au, col=cols,lwd=1,lty=3,main="MMR1")
+pdf(file.path(map_dir, "census_immunity_age.pdf"), width=7, height=5)
+plot(au, col=cols,lty=0,main="")
 legend("topright",c("0-85%","86-90%","91-95%","96-100%",">100%","NA")
        ,fill=c(pal,"white"),
        bty="n",inset=0.1,title="Percent immune")
 dev.off()
 #####
 
-pdf(paste("census_immunity_age_hist.pdf"), width=7, height=5)
+pdf(file.path(map_dir, "census_immunity_age_hist.pdf"), width=7, height=5)
 hist(data_au$pc, breaks=50,col="grey", xlab="Proportion vaccinated",main="")
 abline(v=.95,col="red",lty=3,lwd=3)
 abline(v=median(data_au$pc,na.rm=T),col="orange",lty=3,lwd=3)
 text(x=c(0.835,.85),y=c(200,200),c("median = ",round(median(data_au$pc,na.rm=T),2)))
  dev.off()
 
-###
-
-## Christchurch
-pdf(paste("census_immunity_age_ChCh.pdf"), width=7, height=5)
-
 #####
+main_centers <- read.table(header=TRUE, text = "
+      City xmin    xmax    ymin    ymax
+      ChCh 1550461 1559320 5141317 5212140
+      Auk  1693320 1803320 5892140 5952140
+      Well 1730320 1784500 5421140 5444400")
 
-plot(au, col=cols,lwd=1,lty=3,main="MMR1",xlim=c(1550461 , 1559320),ylim=c(5141317 , 5212140)) # Christchurch
-legend("topright",c("0-85%","86-90%","91-95%","96-100%",">100%","NA")
-       ,fill=c(pal,"white"),
-       # bty="n",inset=0.0,
-       title="Percent immune",bg="white",box.col="white")
- dev.off()
-
-
-## Auckland
-
-pdf(paste("census_immunity_age_Auk.pdf"), width=7, height=5)
-
-plot(au, col=cols,lwd=1,lty=3,main="MMR1",xlim=c( 1693320, 1803320),ylim=c(5892140, 5952140)) # auckland
-legend("topright",c("0-85%","86-90%","91-95%","96-100%",">100%","NA")
-       ,fill=c(pal,"white"),
-       # bty="n",inset=0.0,
-       title="Percent immune",bg="white",box.col="white")
- dev.off()
-
-## Wellington
-
- pdf(paste("census_immunity_age_Well.pdf"), width=7, height=5)
-
-plot(au, col=cols,lwd=1,lty=3,main="MMR1",xlim=c( 1730320, 1784500),ylim=c(5421140, 5444400)) # wellington
-legend("topright",c("0-85%","86-90%","91-95%","96-100%",">100%","NA")
-       ,fill=c(pal,"white"),
-       # bty="n",inset=0.0,
-       title="Percent immune",bg="white",box.col="white")
-dev.off()
+for (center in 1:nrow(main_centers)) {
+  map_file <- paste0("census_immunity_age_", main_centers$City[center], ".pdf")
+  pdf(file.path(map_dir, map_file), width=7, height=5)
+  
+  xlim <- as.numeric(main_centers[center, c("xmin", "xmax")])
+  ylim <- as.numeric(main_centers[center, c("ymin", "ymax")])
+  plot(au, col=cols, lty=0, xlim=xlim, ylim=ylim)
+  legend("topright",c("0-85%","86-90%","91-95%","96-100%",">100%","NA")
+         ,fill=c(pal,"white"),
+         # bty="n",inset=0.0,
+         title="Percent immune",bg="white",box.col="white")
+  dev.off()
+}
 
 ##############
 res_age<- testgp[,c(2:17,19:33)]
