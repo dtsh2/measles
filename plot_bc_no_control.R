e9043f4f31a116e51a44c5d7d71a572253fc3f3b
dtsh2 <davidtshayman@gmail.com>
2017-06-06 16:20:24 +1200

    figures and tables for plots

--- plot_bc_no_control.R
+++ plot_bc_no_control.R
@@ -17,8 +17,26 @@ for (i in 1:length(sample))
 
 bc_res <- sapply(1:length(result), function(i) as.numeric(result[[i]][21,11]))
 #bc_res
-plot(bc_res,xaxt='n',ylim=c(0,max(bc_res)), type='l')
-axis(1, at=1:length(sample), labels=signif(sample,2),tick=F)
-abline(h=1, col='red',lty=3)
+pdf(file.path(dhb_fig_path, paste("bc_no.pdf")))
+
+plot(bc_res,xaxt='n',ylim=c(0,max(bc_res)), col='red',lwd=2,type='l',ylab="benefit/cost ratio",xlab="Cost")
+#axis(1, at=1:length(sample), labels=signif(sample,2),tick=F)
+axis(1, at=axTicks(1), labels=sprintf("$%s", axTicks(1)))
+
+segments(x0=1667, y0=1, x1 = 0, y1 = 1)
+segments(x0=1667, y0=0, x1 = 1667, y1 = 1)
+
+par(new=TRUE)
+par(oma=c(1,2,4,1))
+par(mfcol=c(2,2), mfg=c(1,2))
+par(mar=c(2,2,2,2))
+
+plot(bc_res,xaxt='n',ylim=c(0,max(bc_res)), xlim=c(0,200),col='red',lwd=2,type='l',ylab="",xlab="")
+#axis(1, at=1:length(sample), labels=signif(sample,2),tick=F)
+axis(1, at=axTicks(1), labels=sprintf("$%s", axTicks(1)))
+segments(x0=1667, y0=1, x1 = 0, y1 = 1)
+
+dev.off()
+
 b<-as.data.frame(bc_res)
 which.min(abs(b$bc_res - 1))
